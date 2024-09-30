2805bf3e5017c64f3986633422e71bdfbea55a5f
Rodrigo Avila <rodrigo@Rodrigos-MacBook-Pro-3.local>
2024-09-30 11:07:42 -0300

    refactor menu state

--- src/view/world_cups_view.py
+++ src/view/world_cups_view.py
@@ -10,29 +10,33 @@ from view.abstract_view_strategy import AbstractViewStrategy, ViewStrategy
 import streamlit as st
 from streamlit_option_menu import option_menu
 
-class WorldCupsViewOption(Enum):
-    TEAM = "Team"
-    MATCH = "Match"
-    PLAYER = "Player"
-    
-    def to_list() -> List:
-        return [e.value for e in WorldCupsViewOption]
+
 
 #How to preserve session state in Streamlit: https://discuss.streamlit.io/t/multi-page-apps-with-widget-state-preservation-the-simple-way/22303/8
 class WorldCupsView(AbstractViewStrategy):
     logger = logging.getLogger(__name__)
     
+    class MenuOption(Enum):
+        TEAM = "Team"
+        MATCH = "Match"
+        PLAYER = "Player"
+    
+        def to_list() -> List:
+           return [e.value for e in WorldCupsView.MenuOption]
+    
     class State:
         @staticmethod
-        def set_view_menu_option(menu_option: WorldCupsViewOption | None) -> None:
-            if 'world_cup_view_menu_option' not in st.session_state or menu_option is None:
-                st.session_state['world_cups_view_menu_option'] = WorldCupsViewOption.TEAM
-            st.session_state['world_cups_view_menu_option'] = st.session_state['world_cups_view_menu_option']
+        def set_view_menu_option(menu_option: str) -> None:
+            if menu_option not in WorldCupsView.MenuOption.to_list():
+                raise ValueError(f"Invalid menu option: {menu_option}")
+
+            st.session_state['world_cups_view_menu_option'] = menu_option
             
         @staticmethod
-        def get_view_menu_option() -> WorldCupsViewOption:
+        def get_view_menu_option() -> str:
             if 'world_cups_view_menu_option' not in st.session_state:
-                WorldCupsView.State.set_view_menu_option(WorldCupsViewOption.TEAM)
+                WorldCupsView.State.set_view_menu_option(WorldCupsView.MenuOption.TEAM.value)
+            
             return st.session_state['world_cups_view_menu_option']
     
     def __init__(
@@ -49,39 +53,36 @@ class WorldCupsView(AbstractViewStrategy):
     def render(self) -> None:
         st.title("World Cup Analysis")
         
-        
         menu_option = self.option_menu_fragment()
-        
-      
         competitions = self.get_cached_competitions()
         competition_name, season_name, competition = SelectBoxes.select_competition_and_season(competitions)
         matches = self.get_cached_matches(competition_name, season_name, competition)
         team_name = SelectBoxes.team_select(matches)
         team_matches = self.get_cached_team_matches(competition_name, season_name, matches, team_name)
         
-        if menu_option == WorldCupsViewOption.TEAM:
+        if menu_option == self.MenuOption.TEAM:
             self.team_fragment()
-        elif menu_option == WorldCupsViewOption.MATCH:
+        elif menu_option == self.MenuOption.MATCH:
             self.match_fragment(team_matches)
-        elif menu_option == WorldCupsViewOption.PLAYER:
+        elif menu_option == self.MenuOption.PLAYER:
             self.player_fragment() 
 
-    @st.fragment
     def option_menu_fragment(self):
         menu_index = 0
         if 'world_cups_view_menu_option' in st.session_state:
-            menu_index = WorldCupsViewOption.to_list().index(st.session_state['world_cups_view_menu_option'])
+            menu_index = self.MenuOption.to_list().index(self.State.get_view_menu_option())
 
         menu_option = option_menu(
             None, 
-            options=WorldCupsViewOption.to_list(), 
+            options=self.MenuOption.to_list(), 
             icons=['house', 'cloud-upload', "list-task"], 
             menu_icon="cast",            
             default_index=menu_index,
-            orientation="horizontal"
+            orientation="horizontal",
+            key="world_cups_view_menu"
         )
         
-        st.session_state['world_cups_view_menu_option'] = menu_option
+        self.State.set_view_menu_option(menu_option)
         return menu_option
 
     def team_fragment(self) -> None:
@@ -106,8 +107,8 @@ class WorldCupsView(AbstractViewStrategy):
         competitions = _self.statsbomb_repository.get_competitions()
         
         return competitions[competitions['competition_name'].isin([
-            'FIFA U20 World Cup', 
-            'FIFA World Cup', 
+            "FIFA U20 World Cup", 
+            "FIFA World Cup", 
             "Women's World Cup"
         ])]
         
